563a762f0ed804af35d409c3b53d403484731f4e
tekclick <arman.a@hotmail.fr>
2021-02-28 16:25:20 +0100

    V0.5
    Sound element
    Sliding movement and animation
    Sliding soung

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ const dino = document.getElementById("dino");
 const obstacle1 = document.getElementById("gamestart");
 var scoreCount = document.getElementById("scoreCount");
 var counter = 0;
-var level = 1;
+var speed = 1;
 var soundLevelUp = document.getElementById("audio");
+var soundSlide = document.getElementById("audio2");
+
 // Game Start : launch obstacle on key up press
 window.addEventListener("keydown", (start) => {
   if (start.keyCode == 32) {
@@ -19,28 +21,49 @@ window.addEventListener("keydown", (start) => {
 // Jump function for Doge Runner
 function jump() {
   if (dino.classList != "jump") {
+    //playAudio();
     dino.classList.add("jump");
-
     setTimeout(function() {
       dino.classList.remove("jump")
     }, 280);
-    levelUp();
+  }
+}
+
+// Slide ACTION
+function slide(){
+  if (dino.classList !="slide"){
+    // playSlide();
+    dino.classList.add("slide");
+    setTimeout(function(){
+      dino.classList.remove("slide")
+    }, 400);
   }
 }
 
 // LEVEL UP function
-function levelUp() {
-  if (counter > 300) {
-    level = 2;
+document.body.onkeyup = function(n){
+    if(n.keyCode == 38){
+      levelUp();
+    }
+}
 
+var obstacle1LeftPosition = parseInt(window.getComputedStyle(obstacle1).getPropertyValue("left"));
+function levelUp() {
+  if (counter > 100) {
+    document.getElementById("gamestart").style.animationDuration = "0.8s";
+    console.log("level calculation");
   }
 }
+
 
 // Play sound FUNCTIONS
 function playAudio(){
   soundLevelUp.play();
 }
 
+function playSlide(){
+  soundSlide.play();
+}
 
 // GAME RULES : die if collision
 let isAlive = setInterval(function() {
@@ -54,10 +77,10 @@ let obstacle1Left = parseInt(window.getComputedStyle(obstacle1).getPropertyValue
   // detect colision
   if (obstacle1Left < 50 && obstacle1Left > 0 && dinoTop >= 160) {
     // collision
-    playAudio();
     alert("game over. " + "Your score is " + Math.floor(counter / 1) + "m run.");
     scoreCount.innerHTML = counter;
-    obstacle1.classList.remove("obstacle1")
+    obstacle1.classList.remove("obstacle1");
+    document.getElementById("gamestart").style.animationDuration = "1.3s";
     counter = 0;
     scoreCount.innerHTML = "384400000";
 
@@ -73,6 +96,11 @@ let obstacle1Left = parseInt(window.getComputedStyle(obstacle1).getPropertyValue
 document.body.onkeydown = function(e) {
   if (e.keyCode == 38) {
     jump();
+  }
+  else {
+    if (e.keyCode == 40){
+      slide();
+    }
   }
 };
 
